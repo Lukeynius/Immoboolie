977c5de810a9e86de64787e013dfe15ce2d83b92
Lukeynius <lukas.prager11@gmail.com>
2026-01-03 22:29:42 +0100

    updated CS-File SofaFurniturestate and added DrawerFurniturestate

--- Assets/DrawerFurniturestate.cs
+++ Assets/DrawerFurniturestate.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class SofaFurniturestate : MonoBehaviour
+public class DrawerFurniturestate : MonoBehaviour
 {
     public enum Furniturestatus { Clean, Dirty }
     public Furniturestatus status = Furniturestatus.Clean;
