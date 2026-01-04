4ca4335c24bcbf73ca1c3c6a58bc87a192ad5454
Lukeynius <lukas.prager11@gmail.com>
2026-01-04 16:59:23 +0100

    added Statesrcipts

---
+++ Assets/Scripts/KitchenCabinetRightFurniturestate.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class KitchenCabinetRightFurniturestate : MonoBehaviour
+{
+    public enum Furniturestatus { Clean, Dirty }
+    public Furniturestatus status = Furniturestatus.Clean;
+
+    public GameObject cleanModel;
+    public GameObject dirtyModel;
+
+    void Start()
+    {
+        UpdateVisual();
+    }
+
+    public void SetDirty()
+    {
+        status = Furniturestatus.Dirty;
+        UpdateVisual();
+    }
+
+    public void SetClean()
+    {
+        status = Furniturestatus.Clean;
+        UpdateVisual();
+    }
+
+    void UpdateVisual()
+    {
+        cleanModel.SetActive(status == Furniturestatus.Clean);
+        dirtyModel.SetActive(status == Furniturestatus.Dirty);
+    }
+}
